e70c675267df9822fb935850efbe4936c627bd83
David Jungst <djungst@gmail.com>
2018-09-19 10:00:34 -0600

    server side code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = [
+  'Games',
+  'Health & Fitness',
+  'Business',
+  'Lifestyle',
+  'Entertainment',
+  'Sports'
+]
+
+featured = [true, false]
+
+100.times do
+  App.create(
+    name: Faker::App.name,
+    description: Faker::Lorem.paragraph(4),
+    version: Faker::App.version,
+    author: Faker::App.author,
+    category: categories.sample,
+    price: Faker::Commerce.price.to_f,
+    logo: Faker::Company.logo,
+    featured: featured.sample
+  )
+end
+
+
+
+
+
+
+
